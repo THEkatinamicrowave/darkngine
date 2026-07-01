66d8855d5e04fc56c367c0c649e7913d318ec428
KadePleaseHelpMe <cisforcookiemoonster@gmail.com>
2026-06-30 19:11:24 -0500

    it functions

--- source/Main.hx
+++ source/Main.hx
@@ -1,13 +1,15 @@
 package;
 
+import openfl.display.Sprite;
 import flixel.FlxGame;
+
 class Main extends Sprite
 {
 	public function new()
 	{
 		super();
 
-		var game:FlxGame = new FlxGame(1280, 720, MainState);
+		var game:FlxGame = new FlxGame(1280, 720, PlayState);
 		addChild(game);
 	}
 }
