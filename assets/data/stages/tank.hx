e0b2d557bbf4242b89664fa922d4c210b09e734d
KadePleaseHelpMe <cisforcookiemoonster@gmail.com>
2026-07-24 17:16:02 -0400

    week 7 stage NOT CHARACTERS

--- assets/data/stages/tank.hx
+++ assets/data/stages/tank.hx
@@ -1,19 +1,39 @@
+//
+import flixel.math.FlxAngle;
+import flixel.addons.display.FlxTiledSprite;
+
+var clouds:FlxTiledSprite;
+
+var tankMoving:Bool = false;
 var tankAngle:Float = FlxG.random.int(-90, 45);
 var tankSpeed:Float = FlxG.random.float(5, 7);
+var tankX:Float = 400;
 
-function postCreate() if (PlayState.isStoryMode)
-	GameOverSubstate.script = 'data/scripts/week7-balledLines';
+function postCreate() {
+	clouds = new FlxTiledSprite(Paths.image('stages/tank/tankClouds'), 3200, 235, true, false);
+	clouds.setPosition(-1100, 20);
+	clouds.scrollFactor.set(0.25, 0.25);
+	clouds.velocity.x = 8;
+	insert(members.indexOf(buildings), clouds);
 
-function update(elapsed:Float) {
-	tankAngle += elapsed * tankSpeed;
-	tankRolling.angle = tankAngle - 90 + 15;
-	tankRolling.x = 400 + (1500 * Math.cos(Math.PI / 180 * (1 * tankAngle + 180)));
-	tankRolling.y = 1300 + (1100 * Math.sin(Math.PI / 180 * (1 * tankAngle + 180)));
+	tankAngle = FlxG.random.int(-90, 45);
+	tankSpeed = FlxG.random.float(5, 7);
+
+	moveTank(0);
+
+	if (PlayState.isStoryMode)
+		GameOverSubstate.script = 'data/scripts/week7-balledLines';
 }
 
-function onPostCountdown(event) {
-	if (!playCutscenes || event.sprite == null) return;
-	var lastIndex:Int = members.indexOf(event.sprite);
-	remove(event.sprite);
-	insert(lastIndex + 4, event.sprite);
+function postUpdate(elapsed:Float) {
+	moveTank(elapsed);
+}
+
+function moveTank(elapsed:Float) {
+	var daAngleOffset:Float = 1;
+	tankAngle += elapsed * tankSpeed;
+
+	tankRolling.angle = tankAngle - 90 + 15;
+	tankRolling.x = tankX + Math.cos(FlxAngle.asRadians((tankAngle * daAngleOffset) + 180)) * 1500;
+	tankRolling.y = 1300 + Math.sin(FlxAngle.asRadians((tankAngle * daAngleOffset) + 180)) * 1100;
 }
