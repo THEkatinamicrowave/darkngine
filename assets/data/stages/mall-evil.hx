fd5456cac551ae81f0fc5091757bc3053823ee7a
KadePleaseHelpMe <cisforcookiemoonster@gmail.com>
2026-07-24 01:20:45 -0400

    update the vignette shader

--- assets/data/stages/mall-evil.hx
+++ assets/data/stages/mall-evil.hx
@@ -1,24 +1,27 @@
-var idk:CustomShader = null;
-function create() {
-	if(!Options.gameplayShaders) {
+//
+var badVignette:CustomShader;
+
+function postCreate() {
+	if (!Options.gameplayShaders) {
 		disableScript();
 		return;
 	}
 
-	idk = new CustomShader('coloredVignette');
-	idk.color = [1, 0, 0];
-	camGame.addShader(idk);
+	badVignette = new CustomShader('coloredVignette');
+	badVignette.color = [1.0, 0.0, 0.0];
+	camGame.addShader(badVignette);
 }
 
 var adder:Int = 0;
-function postUpdate(elapsed:Float) {  // Doing it like this so at least the red bump doesn't look too much instant
+function postUpdate(elapsed:Float) {
 	var toAdd:Int = lerp(adder, 0, elapsed);
-	idk.amount = camGame.zoom * camHUD.zoom + toAdd;
-	idk.strength = (camGame.zoom - camHUD.zoom + toAdd) * 5;
-	if(adder > 0) adder -= 0.0001 * camZoomingStrength;
+	badVignette.amount = camGame.zoom * camHUD.zoom + toAdd;
+	badVignette.strength = (camGame.zoom - camHUD.zoom + toAdd) * 5;
+	
+	if (adder > 0) adder -= (0.0001 * camZoomingStrength);
 }
 
-function beatHit() {
-	if(Options.camZoomOnBeat && camZooming && FlxG.camera.zoom < maxCamZoom && curBeat % camZoomingInterval == 0)
+function beatHit(beat:Int) {
+	if (Options.camZoomOnBeat && camZooming && FlxG.camera.zoom < maxCamZoom && curBeat % camZoomingInterval == 0)
 		adder = 0.05 * camZoomingStrength;
 }
