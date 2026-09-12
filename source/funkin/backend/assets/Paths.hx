194d4ba0d1758df63d3fbcf7d0ff6f6aada5d26c
KadePleaseHelpMe <cisforcookiemoonster@gmail.com>
2026-09-12 18:14:23 -0400

    merge "fix 1.png again"

--- source/funkin/backend/assets/Paths.hx
+++ source/funkin/backend/assets/Paths.hx
@@ -313,8 +313,13 @@ class Paths
 
 		if (frameCollections.length == 1 && !unique && (key == null || key == assetKey)) return frameCollections[0];
 
-		asset = new FlxAnimateFrames(FlxGraphic.fromFrame(FlxG.bitmap.whitePixel, unique, assetKey));
-		for (frames in frameCollections) asset.addAtlas(cast frames); // wont compile in hashlink because of mismatch type
+		// this has casting issues if no texture atlas is involved???
+		// cus somehow this crashes for 1.png method
+		//asset = new FlxAnimateFrames(FlxGraphic.fromFrame(FlxG.bitmap.whitePixel, unique, assetKey));
+		//for (frames in frameCollections) asset.addAtlas(cast frames); // wont compile in hashlink because of mismatch type
+		
+		// see FlxAnimateFrames#L495
+		asset = FlxAnimateFrames.combineAtlas(cast frameCollections);
 
 		if (!unique) tempFramesCache.set(assetKey, asset);
 		return asset;
